c55c9eb2efdc50bab318dbf0ff65216ee4103f87
leonardoac12 <leonardoarcr@outlook.es>
2025-10-07 02:10:49 -0500

    act HealthController.java

--- src/main/java/com/leonardoarotinco/portfolio/controllers/HealthController.java
+++ src/main/java/com/leonardoarotinco/portfolio/controllers/HealthController.java
@@ -4,11 +4,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class HealthController {
 
     @GetMapping("/health")
-    public ResponseEntity<String> healthCheck() {
-        return ResponseEntity.ok("Ok");
+    public ResponseEntity<Map<String, String>> healthCheck() {
+        Map<String, String> status = new HashMap<>();
+        status.put("status", "UP");
+        status.put("timestamp", Instant.now().toString());
+        status.put("service", "portfolio-service");
+
+        return ResponseEntity.ok(status);
     }
 }
