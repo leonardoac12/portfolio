50fc14845ff28201227597420186df502633ab62
leonardoac12 <leonardoarcr@outlook.es>
2025-10-07 00:53:02 -0500

    feature healthCheck()

--- src/main/java/com/leonardoarotinco/portfolio/controllers/HomeController.java
+++ src/main/java/com/leonardoarotinco/portfolio/controllers/HomeController.java
@@ -1,13 +1,19 @@
 package com.leonardoarotinco.portfolio.controllers;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
         return "index.html";
+    }
+
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("Ok");
     }
 }
